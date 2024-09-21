2388ab6b4c0046e8bec67f3e57c923aca545cf81
sravankuamrreddy23 <sravan200123@gmail.com>
2024-09-21 02:53:37 -0700

    nvn

---
+++ don.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#Author:bhaskar reddy lacchannagari.
+#Date:7th june 2013
+echo "please enter your name:" read userName
+echo "The name you enterd is $userNameRun the above script as follows:"
