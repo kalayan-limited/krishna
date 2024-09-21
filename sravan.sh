2581ac60c2f4c45e26c30a44a61ddda310e9f7eb
sravankuamrreddy23 <sravan200123@gmail.com>
2024-09-21 02:52:09 -0700

    nvk

---
+++ sravan.sh
@@ -0,0 +1,15 @@
+echo "welcome"
+name="john"
+echo  "hello,$name"
+date="17-09-2024"
+echo "Date is $date "
+a=25
+b=35
+result=$((a+b))
+echo $result
+a=20
+b=10
+result=$((a*b))
+echo $result
+read-p "enter
+echo $age
